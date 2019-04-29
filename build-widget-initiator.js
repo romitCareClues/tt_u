26add49fbe0674045745df6eb3415e00c46f600d
Romit <romit.sanyal@careclues.com>
2019-04-29 21:17:56 +0530

    WIP - setting environment for widget initiator completed

--- build-widget-initiator.js
+++ build-widget-initiator.js
@@ -19,42 +19,44 @@ const finalPath = './dist/doctor-widget/assets/js/widget-initiator.min.js';
     await fs.ensureDir('./dist/doctor-widget/assets/js');
     await concat(files, tempDestinationPath).then((result) => {
         console.log('tempDestinationPath - success');
-        minify({
-            compressor: gcc,
-            input: tempDestinationPath,
-            output: tempDestinationPath2,
-            sync: true,
-            callback: function (err, min) {
-                if (!err) {
-                    console.log('tempDestinationPath2 - success');
-                    minify({
-                        compressor: uglifyjs,
-                        input: tempDestinationPath2,
-                        output: finalPath,
-                        callback: function (err, min) {
-                            if (!err) {
-                                fs.readFile(finalPath, 'utf8', function (err, data) {
-                                    if (err) {
-                                        return console.log(err);
-                                    }
-                                    var result = data.replace(`var ENV = "development";`, `var ENV = "${environment}";`);
 
-                                    fs.writeFile(finalPath, result, 'utf8', function (err) {
-                                        if (err) return console.log(err);
-                                        else return console.log('finalPath - success' + environment);
-                                    });
-                                });
-                            }
-                            else {
-                                console.log('finalPath_error -> ' + err);
-                            }
-                        }
-                    });
-                }
-                else {
-                    console.log('tempDestinationPath2_error -> ' + err);
-                }
+        fs.readFile(tempDestinationPath, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
             }
+            var result = data.replace(`var ENV = "development";`, `var ENV = "${environment}";`);
+
+            fs.writeFile(tempDestinationPath, result, 'utf8', function (err) {
+                if (err) return console.log(err);
+
+                minify({
+                    compressor: gcc,
+                    input: tempDestinationPath,
+                    output: tempDestinationPath2,
+                    sync: true,
+                    callback: function (err, min) {
+                        if (!err) {
+                            console.log('tempDestinationPath2 - success');
+                            minify({
+                                compressor: uglifyjs,
+                                input: tempDestinationPath2,
+                                output: finalPath,
+                                callback: function (err, min) {
+                                    if (!err) {
+                                        console.log('finalPath - success');
+                                    }
+                                    else {
+                                        console.log('finalPath_error -> ' + err);
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            console.log('tempDestinationPath2_error -> ' + err);
+                        }
+                    }
+                });
+            });
         });
     });
 })()
