7c9954b196bef3c2cf7c9321f9d68832b69ad309
Romit <romit.sanyal@careclues.com>
2019-09-17 16:15:53 +0530

    clear loca storage whenever widget is instantiated
    proper removal of dom elements for widget when closed

--- init-widget/init-widget.js
+++ init-widget/init-widget.js
@@ -221,14 +221,13 @@ function removeWidget() {
     // document.getElementById(WIDGET_ID).remove();
     let widgetPlaceHolderElement = document.getElementById('careclues_widget_container');
     widgetPlaceHolderElement.innerHTML = "";
-    clearLocalStorage();
 }
 
-function clearLocalStorage() {
-    let storageKeys = [
-        'cc_appointment_date', 'cc_appointment_type_key', 'cc_auth_token',
-        'cc_clinic_id', 'cc_clinic_slug', 'cc_doctor', 'cc_patient_id',
-        'cc_saved_appointment', 'cc_schedule', 'cc_selected_slot', 'cc_visit_type_title'
-    ];
-    storageKeys.forEach((storageKey) => { localStorage.removeItem(storageKey); });
-}
+// function clearsessionStorage() {
+    // let storageKeys = [
+    //     'cc_appointment_date', 'cc_appointment_type_key', 'cc_auth_token',
+    //     'cc_clinic_id', 'cc_clinic_slug', 'cc_doctor', 'cc_patient_id',
+    //     'cc_saved_appointment', 'cc_schedule', 'cc_selected_slot', 'cc_visit_type_title'
+    // ];
+    // storageKeys.forEach((storageKey) => { sessionStorage.removeItem(storageKey); });
+// }
